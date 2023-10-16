7b6ec72f4be639714920a6b03874715ff5a75860
Ksenia Agalakova <agalaksu111@gmail.com>
2023-10-16 20:30:34 +0300

    Product feed layout changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,11 @@
 import Head from "next/head";
 import Header from "../components/Header";
 import Banner from "../components/Banner";
+import ProductFeed from "../components/ProductFeed";
 
-export default function Home() {
+export default function Home({ products }) {
   return (
-    <div>
+    <div className="bg-gray-100">
       <Head>
         <title>Amazon 2.0</title>
       </Head>
@@ -16,7 +17,18 @@ export default function Home() {
         <Banner />
 
         {/*Product Feed*/}
+        <ProductFeed products={products} />
       </main>
     </div>
   );
+}
+
+export async function getServerSideProps(context) {
+  const products = await fetch("https://fakestoreapi.com/products").then((res) => res.json());
+
+  return {
+    props: {
+      products,
+    },
+  };
 }
