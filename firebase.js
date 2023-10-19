038c50f1a9a1dbf125d3cc75832dfd57921940d1
Ksenia Agalakova <agalaksu111@gmail.com>
2023-10-19 22:16:32 +0300

    Set up Next-auth and Firebase auth

---
+++ firebase.js
@@ -0,0 +1,10 @@
+import Firebase from "firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD8mZebQZiO4oHvpD9U41TH96QHDrrsroo",
+  authDomain: "fir-a242d.firebaseapp.com",
+  projectId: "fir-a242d",
+  storageBucket: "fir-a242d.appspot.com",
+  messagingSenderId: "116985266221",
+  appId: "1:116985266221:web:4797473f6e54d8d14938a4",
+};
