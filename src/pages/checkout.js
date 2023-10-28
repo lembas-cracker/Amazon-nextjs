9035514eb12333953cf9c036cba46abdbc70c354
Ksenia Agalakova <agalaksu111@gmail.com>
2023-10-29 02:03:19 +0300

    Checkout page: check auth and display price

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -1,11 +1,14 @@
 import Header from "../components/Header";
 import Image from "next/image";
 import { useSelector } from "react-redux";
-import { selectItems } from "../slices/basketSlice";
+import { selectItems, selectTotal } from "../slices/basketSlice";
 import CheckoutProduct from "../components/CheckoutProduct";
+import { useSession } from "next-auth/react";
 
 const Checkout = () => {
   const items = useSelector(selectItems);
+  const total = useSelector(selectTotal);
+  const { data: session } = useSession();
 
   return (
     <div className="bg-gray-100">
@@ -35,7 +38,26 @@ const Checkout = () => {
           </div>
         </div>
 
-        <div></div>
+        {/*Right section */}
+        <div className="flex flex-col bg-white p-10 shadow-md">
+          {items.length > 0 && (
+            <>
+              <h2 className="whitespace-nowrap">
+                Subtotal ({items.length} items):
+                <span className="font-bold"> ${total}</span>
+              </h2>
+
+              <button
+                disabled={!session}
+                className={`button mt-2 ${
+                  !session && "from-gray-300 to-gray-500 border-gray-200 text-gray-200 cursor-not-allowed"
+                }`}
+              >
+                {!session ? "Sign in to checkout" : "Proceed to checkout"}
+              </button>
+            </>
+          )}
+        </div>
       </main>
     </div>
   );
