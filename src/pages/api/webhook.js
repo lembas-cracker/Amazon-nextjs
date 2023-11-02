5f37a9009f5fc255b166c29a014e25ebc936d9e6
Ksenia Agalakova <agalaksu111@gmail.com>
2023-11-02 21:12:08 +0300

    Webhook and Stripe Checkout Session

---
+++ src/pages/api/webhook.js
@@ -0,0 +1,23 @@
+import { buffer } from "micro";
+import * as admin from "firebase-admin";
+
+const serviceAccount = require("../../../permissions.json");
+
+//Secure a connection to Firebase from the backend
+const app = !admin.apps.length
+  ? admin.initializeApp({
+      credential: admin.credential.cert(serviceAccount),
+    })
+  : admin.app();
+
+//Establish connection to Stripe
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+const endpointSecret = process.env.STRIPE_SIGNING_SECRET;
+
+export default async (req, res) => {
+  if (req.method === "POST") {
+    const requestBuffer = await buffer(req);
+    const payload = requestBuffer.toString();
+    const sig = req.headers("stripe-signature");
+  }
+};
