e812fa2fe8df1262d412cfe95497a3c9ec04d9ed
Ksenia Agalakova <agalaksu111@gmail.com>
2023-11-29 16:30:06 +0300

    Removed unneeded console.log

--- src/pages/api/create-checkout-session.js
+++ src/pages/api/create-checkout-session.js
@@ -37,7 +37,6 @@ export default async function handler(req, res) {
           images: JSON.stringify(items.map((item) => item.image)),
         },
       });
-      console.log(session.url, process.env.HOST);
       res.redirect(303, session.url);
     } catch (err) {
       res.status(err.statusCode || 500).json(err.message);
